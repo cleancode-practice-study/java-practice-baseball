1486acf7dc02c2c74dca89b4714df470a9e61ee5
gch01410 <gch01410@naver.com>
2021-03-28 01:25:50 +0900

    docs: 미션 요구사항 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Application {
 	public static void main(String[] args) {
-		// 여기에 구현
+		final Scanner scanner = new Scanner(System.in);
+
+		// TODO 구현 진행
 
 	}
 }
