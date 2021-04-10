850a05bc4ed7f2cb1fb31432ff1b2b05eb265968
limhalim <gkfla668@naver.com>
2021-04-10 19:05:14 +0900

    my fourth commit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,22 +1,27 @@
 import java.util.Scanner;
 
 public class Application {
-	public static final int REPLAY = 1;
-	public static final int CLOSE = 2;
-	public static void main(String[] args) {
-		final Scanner scanner = new Scanner(System.in);
-		BaseballGame baseballgame = new BaseballGame();
-		boolean run = true;
+    public static final int REPLAY = 1;
+    public static final int CLOSE = 2;
 
-		while (run) {
-			baseballgame.start();
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
 
-			int game = scanner.nextInt();
-			if (game == REPLAY) {
-				run = true;
-			} else if (game == CLOSE) {
-				run = false;
-			}
-		}
-	}
+        BaseballGame baseballgame = new BaseballGame();
+
+        int game;
+        boolean run = true;
+
+        while (run) {
+            baseballgame.start();
+
+            game = scanner.nextInt();
+
+            if (game == REPLAY) {
+                run = true;
+            } else if (game == CLOSE) {
+                run = false;
+            }
+        }
+    }
 }
--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,20 +1,34 @@
+import com.sun.tools.javac.util.StringUtils;
+
 import java.util.Scanner;
 
 public class BaseballGame {
-    public static final int MAX_STRIKE = 3;
+    public static final int NUMBER_PLAYER = 3;
+
     final Scanner scanner = new Scanner(System.in);
+
+    private String guessNum = "";
+
     int computer[] = new int[3];
     int player[] = new int[3];
-    private String guessNum = "";
+
     private int strike = 0;
     private int ball = 0;
 
-    public void guessNumber(){
-        guessNum = scanner.next(); // String 으로 숫자 입력 받은 뒤
-        String[] array = guessNum.split(""); // string 배열로 변환
+    public void guessNumber() {
+        guessNum = scanner.next();
 
-        for(int i = 0; i < player.length; i++){
-            player[i] = Integer.parseInt(array[i]); // string 배열을 int 배열로 변환
+        int stringNumber = guessNum.length();
+
+        if (stringNumber != NUMBER_PLAYER) {
+            System.out.println("1부터 9까지의 정수 3자리를 입력해주세요.");
+            guessNum = scanner.next();
+        }
+
+        String[] changeArray = guessNum.split("");
+
+        for (int i = 0; i < player.length; i++) {
+            player[i] = Integer.parseInt(changeArray[i]);
         }
     }
 
@@ -24,38 +38,60 @@ public class BaseballGame {
 
         for (int i = 0; i < computer.length; i++) {
             for (int j = 0; j < player.length; j++) {
-                if (computer[i] == player[j] && i == j) {
-                    strike++;
-                } else if (computer[i] == player[j] && i != j) {
-                    ball++;
-                }
+                compare(i, j);
             }
         }
 
         if (strike > 0 && ball > 0)
             System.out.println(ball + "볼" + strike + "스트라이크\n");
-        else if(strike == 0 && ball > 0)
+        else if (strike == 0 && ball > 0)
             System.out.println(ball + "볼");
-        else if(strike > 0 && ball == 0)
+        else if (strike > 0 && ball == 0)
             System.out.println(strike + "스트라이크");
         else if (strike == 0 && ball == 0)
             System.out.println("Nothing");
     }
 
+    public void compare(int i, int j) {
+        if (computer[i] == player[j] && i == j)
+            strike++;
+        else if (computer[i] == player[j] && i != j)
+            ball++;
+    }
+
+    /* public void overLab(int i, int j) {
+        if (computer[i] == computer[j]) {
+            computer[i] = (int) (Math.random() * 9) + 1;
+        }
+        j--;
+    }
+    */
+
     public void start() {
+        /*
+        int i = 0;
+        int j = 0;
+
+        for (i = 0; i < computer.length; i++) {
+            computer[i] = (int) (Math.random() * 9) + 1;
+            j = i - 1;
+            while (j > 0) {
+                overLab(i, j);
+            }
+        }
+        */
+
         for (int i = 0; i < computer.length; i++) {
             computer[i] = (int) (Math.random() * 9) + 1;
         }
 
-        System.out.println("1부터 9까지의 정수를 입력해주세요.");
+        do {
+            System.out.println("숫자를 입력해주세요 : ");
+            guessNumber();
+            gameResult();
+        } while (strike != NUMBER_PLAYER);
 
-            do {
-                System.out.println("숫자를 입력해주세요 : ");
-                guessNumber();
-                gameResult();
-            }while (strike != MAX_STRIKE);
-
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
